98db15128539b42678a39787809ce0b6c88acd5c
Teodor Calin <tedcn02@yahoo.com>
2019-11-14 00:55:42 +0100

    JSON array

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
+#include <vector>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
 
@@ -14,10 +16,42 @@ int main()
     t.put("fw ver", "1260090");
     t.put("model", "XRPAD3");
     t.put("gen", 2);
+
     cout << t.get_child("sw ver").get_value<string>() << endl;
     cout << t.get<int>("gen") << endl;
     ostringstream oss;
     pt::write_json(oss, t);
     cout << oss.str() << endl;
+
+
+    vector<string> fruits {"apple", "raspberry", "orange"};
+    map<string, string> animals { {"rabbit", "white"}, {"dog", "brown"}, {"cat", "grey"} };
+
+    pt::ptree root_node;
+
+    pt::ptree animals_node;
+    for(auto& animal : animals)
+        animals_node.put(animal.first, animal.second);
+    root_node.add_child("animals", animals_node);
+
+    pt::ptree fruits_node;
+    for(auto& fruit : fruits)
+    {
+        pt::ptree fruit_node;
+        fruit_node.put("", fruit);
+        fruits_node.push_back(make_pair("",fruit_node));
+    }
+    root_node.add_child("fruits", fruits_node);
+
+    pt::ptree fish_node;
+    auto fishes =  {"bream", "pike", "trout", "salmon"};
+    for (auto& fish : fishes)
+    {
+        fish_node.push_back(make_pair("fish", pt::ptree(fish)));
+    }
+    root_node.add_child("fish", fish_node);
+
+    pt::write_json(cout, root_node);
+
     return 0;
 }
